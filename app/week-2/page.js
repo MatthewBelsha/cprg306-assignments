cce713b5d24e77b90f5fa76e7759eef13073ec3b
Matthew Belsham <matthewbels@gmail.com>
2025-09-11 09:29:54 -0600

    Basic next.js project

---
+++ app/week-2/page.js
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+export default function Page() {
+  return (
+    <main>
+      <h1>Shopping List</h1>
+      <h1>Matthew Belsham</h1>
+      <p>
+        <Link href="https://github.com/MatthewBelsha">
+          https://github.com/MatthewBelsha
+        </Link>
+      </p>
+    </main>
+  );
+}
---
+++ app/week-2/student-info.js
@@ -0,0 +1,7 @@
+const { default: Link } = require("next/link");
+
+<main>
+  <title>Student Info</title>
+  <h2>Matthew Belsham</h2>
+  <Link href="https://github.com/MatthewBelsha?tab=repositories">GitHub</Link>
+</main>;
