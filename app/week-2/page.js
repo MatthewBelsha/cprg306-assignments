7d8d84df6d8741795d1960649d30ea2a5d19fdfd
Matthew Belsham <matthewbels@gmail.com>
2025-09-11 09:38:44 -0600

    added group member names

--- app/week-2/page.js
+++ app/week-2/page.js
@@ -4,7 +4,9 @@ export default function Page() {
   return (
     <main>
       <h1>Shopping List</h1>
-      <h1>Matthew Belsham</h1>
+      <h1>Matthew Belsham </h1>
+      <h1>Magnus Mullen</h1>
+      <h1>Benjamin Noel</h1>
       <p>
         <Link href="https://github.com/MatthewBelsha">
           https://github.com/MatthewBelsha
