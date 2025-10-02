7ade49ffd74aa8253d3a7a880961d55af1c0d98f
Matthew Belsham <matthewbels@gmail.com>
2025-10-02 08:09:09 -0600

    Update page.js

--- app/page.js
+++ app/page.js
@@ -14,11 +14,6 @@ export default function Home() {
           <li>
             <Link href="./week-4">Week 4 Assignment</Link>
           </li>
-          <li>
-            <Link href="./week-4" target="_blank" rel="noopener noreferrer">
-              Week 4 Assignment
-            </Link>
-          </li>
         </ul>
       </div>
     </main>
