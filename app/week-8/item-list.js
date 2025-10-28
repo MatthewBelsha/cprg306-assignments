41e3f4cd91abab18a0b386c2f6d25594e0123191
launchland <benjulian.noel@gmail.com>
2025-10-28 09:17:47 -0600

    Week 8 Added

---
+++ app/week-8/item-list.js
@@ -0,0 +1,53 @@
+"use client";
+import { useState } from "react";
+import Item from "./item";
+
+export default function ItemList({ items }) {
+  const [sortBy, setSortBy] = useState("name");
+
+  let sortedItems = [...items]; // copy prop
+
+  if (sortBy === "name") {
+    sortedItems.sort((a, b) => a.name.localeCompare(b.name));
+  } else if (sortBy === "category") {
+    sortedItems.sort((a, b) => a.category.localeCompare(b.category));
+  }
+
+  // button styles
+  const activeClass = "bg-blue-700 text-white border-2 border-white";
+  const inactiveClass = "bg-blue-900 text-white";
+
+  return (
+    <main className="flex flex-col items-center">
+      <div className="flex gap-2 mb-4">
+        <button
+          onClick={() => setSortBy("name")}
+          className={`px-3 py-1 rounded-lg ${
+            sortBy === "name" ? activeClass : inactiveClass
+          }`}
+        >
+          Sort by Name
+        </button>
+        <button
+          onClick={() => setSortBy("category")}
+          className={`px-3 py-1 rounded-lg ${
+            sortBy === "category" ? activeClass : inactiveClass
+          }`}
+        >
+          Sort by Category
+        </button>
+      </div>
+
+      <ul>
+        {sortedItems.map((item) => (
+          <Item
+            key={item.id}
+            name={item.name}
+            quantity={item.quantity}
+            category={item.category}
+          />
+        ))}
+      </ul>
+    </main>
+  );
+}
---
+++ app/week-8/page.js
@@ -0,0 +1,32 @@
+"use client";
+import { useState } from "react";
+import ItemList from "./item-list";
+import NewItem from "./new-item";
+import itemsData from "./items.json";
+
+export default function Page() {
+  const [items, setItems] = useState(itemsData);
+
+  // item handler
+  const handleAddItem = (newItem) => {
+    //spread operator to add new item to existing items
+    //reference https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Spread_syntax
+    setItems((prevItems) => [...prevItems, newItem]);
+  };
+
+  return (
+    <main className="flex flex-col items-center justify-center">
+      <h1 className="p-2 m-3 text-center text-3xl font-bold">Shopping List & Meal Ideas</h1>
+
+      {/*passes on add item*/}
+      <div>
+        <NewItem onAddItem={handleAddItem} />
+      </div>
+
+      {/*current items*/}
+      <div className="space-y-4 p-4 text-center rounded-lg">
+        <ItemList items={items} />
+      </div>
+    </main>
+  );
+}
